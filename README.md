509afafc7abb8ba965ac617b5037b2a706a66535
faisalahammad <faisalahammad24@gmail.com>
2020-04-01 03:18:15 +0600

    First released

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# evaly-cashback-calculator
-Evaly Cashback Calculator to find out cashback amount for full payment &amp; discount payment amount for partial payment. You could simply find out the cashback and payment amount, so no more CONFUSION!!!
+# Evaly Cashback Calculator
+
+"Evaly Cashback Calculator" to find out cashback amount for full payment and discount payment amount for partial payment. You could simply find out the cashback and payment amount, so no more CONFUSION!!!
+
+---
+
+### Screenshot
+50% cashback preview
+![50% Cashback](./images/50.png "50% cashback preview")
+
+
+100% cashback preview
+![50% Cashback](./images/100.png "100% cashback preview")
+
---
+++ assets/main.js
@@ -0,0 +1,59 @@
+let submit = document.querySelector("button#submit");
+let reset = document.querySelector("button#reset");
+
+// Hide by default
+document.querySelector(".message").style.display = "none";
+showHide("none");
+reset.style.display = "none";
+
+// Create function to make it easier
+function quicker(handler, update) {
+  return (document.querySelector(handler).innerText = update);
+}
+function showHide(kihobe) {
+  document.querySelector("#fullPayment").style.display = kihobe;
+  document.querySelector("#partialPayment").style.display = kihobe;
+}
+
+// Get Form data
+submit.addEventListener("click", function(e) {
+  let productPrice = parseFloat(document.querySelector("#proPrice").value);
+  let discountRate = parseFloat(document.querySelector("#discountRate").value);
+
+  // Calculate Discount
+  let discountPayment = Math.round(productPrice / (discountRate / 100 + 1)); // Payment amount
+  let discountGet = productPrice - discountPayment; // Total discount amount
+  let cashbackAmount = productPrice * (discountRate / 100);
+  let cashbackFullPay = productPrice - cashbackAmount;
+
+  if (discountRate == 0 || discountRate > 100) {
+    document.querySelector(".message").style.display = "block";
+    showHide("none"); // Hide
+    setTimeout(function() {
+      document.querySelector(".message").style.display = "none";
+    }, 3000);
+  } else {
+    // Full Payment
+    quicker("#fullPayment .amount", productPrice);
+    quicker("#fullPayment .cashback", cashbackAmount);
+    quicker("#fullPayment .discount", discountRate);
+    quicker("#fullPayment .price", cashbackFullPay);
+
+    // Partial Payment
+    quicker("#partialPayment .amount", discountPayment);
+    quicker("#partialPayment .discount", discountRate);
+    quicker("#partialPayment .price", discountGet);
+
+    showHide("block"); // show
+    reset.style.display = "block";
+  }
+
+  e.preventDefault();
+});
+
+// Reset / Reload page
+reset.addEventListener("click", function() {
+  location.reload();
+  document.querySelector("#proPrice").value = "";
+  document.querySelector("#discountRate").value = "";
+});
